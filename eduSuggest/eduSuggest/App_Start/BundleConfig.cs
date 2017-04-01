9934a4099f4c9d54bb259d770212f8cf907bf2f0
Brandon Mack <bk.mack@yahoo.com>
2017-04-01 02:07:19 -0400

    I added footer to the website

--- eduSuggest/eduSuggest/App_Start/BundleConfig.cs
+++ eduSuggest/eduSuggest/App_Start/BundleConfig.cs
@@ -21,13 +21,13 @@ namespace eduSuggest
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/bootstrap.min.js"));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-spacelab.css",
                       "~/Content/site.css",
-                      "~/Content/"));
+                      "~/Content/bootstrap-social.css",
+                      "~/Content/font-awesome.css"));
         }
     }
 }
